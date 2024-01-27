79b3e15f818875b84a8bd3c6d52f92b56b617aba
SebastianL2 <sebas16cely@gmail.com>
2024-01-27 03:37:02 -0500

    fr

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const path = require('path');
 const bodyParser = require("body-parser");
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 app.use('/', require('./routes/index'))
 app.use(bodyParser.json());
 app.set('PORT', process.env.PORT || 3000)
