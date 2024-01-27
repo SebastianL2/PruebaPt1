96f48f7c86345bcd94c041f2149332511fa50e5b
SebastianL2 <sebas16cely@gmail.com>
2024-01-27 02:59:10 -0500

    e

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const route = require('express').Router()
 
 const path = require('path')
 
-route.get('/',(req,res)=>res.sendFile(path.join(__dirname,'../src/index.html')))
+route.get('/',(req,res)=>res.sendFile(path.join(__dirname,'../views/index.html')))
 
 
 
